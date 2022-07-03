367953b8c1de5970281328e296d98be49c530831
Hextech43 <oluwatoyine86@gmail.com>
2022-07-03 12:58:55 +0100

    this is oja

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express")
 const cors = require("cors")
-const port = 5555
+const port =process.env.PORT || 5555
 const app = express()
 const mongoose = require("mongoose")
 mongoose.connect(process.env.URL).then(()=>{
